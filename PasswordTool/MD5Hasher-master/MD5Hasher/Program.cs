2f61078f9f36904016e00a6718bf49c87959224b
alexanderguminskiy <73988462+alexanderguminskiy@users.noreply.github.com>
2020-12-26 15:04:03 +0300

    Add files via upload

---
+++ PasswordTool/MD5Hasher-master/MD5Hasher/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Security.Cryptography;
+
+namespace MD5Hasher
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            do
+            {
+                String str;
+                byte[] hash;
+                MD5 hasher;
+
+                str = Console.ReadLine();
+                hasher = MD5.Create();
+                hash = Encoding.UTF8.GetBytes(str);
+                hash = hasher.ComputeHash(hash);
+
+                str = "";
+
+                foreach (byte b in hash)
+                {
+                    str = str + b.ToString("x2");
+                }
+
+                Console.WriteLine(str);
+            } while (true);
+            
+
+        }
+    }
+}
